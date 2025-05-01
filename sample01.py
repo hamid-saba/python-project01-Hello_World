3572ef7b254a1aa59467801b57c411f92e04e990
Hamid Saba <hamidabdulfazelsaba@gmail.com>
2025-05-01 14:16:23 +0500

    updated and finished sample01

--- sample01.py
+++ sample01.py
@@ -1,1 +1,16 @@
 from tkinter import *
+
+#window -- has to go first
+root = Tk()
+
+#define then put on screen thats how tkinter works
+#creating a label widget
+myLabel = Label(root, text="Hello World!")
+
+#now put to root widget
+#we can pack it into window widget
+myLabel.pack()
+
+#create an event unit -  a graphic unit loops to notice graphical updates and inputs
+#main loop of program
+root.mainloop()
